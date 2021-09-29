a7835740f311ba42c951bcfd5c4261c13dc80ee3
RafiLingga <90681112+RafiLingga@users.noreply.github.com>
2021-09-29 13:45:53 +0700

    Add files via upload

---
+++ Latihan1.php
@@ -0,0 +1,17 @@
+<?php
+class Latihan1 extends CI_Controller
+{
+    Public Function index()
+    {
+        echo "Selamat Datang.. Selamat Belajar Web Programming";
+        //$this->load->view('View-Latihan1');
+    }
+
+    public function penjumlahan($n1, $n2)
+    {
+            $this->load->model('Model_Latihan1');
+            $data['nilai1'] = $n1; $data['nilai2'] = $n2;
+            $data['hasil'] = $this->Model_Latihan1->jumlah($n1, $n2);
+            $this->load->view('View-Latihan1', $data);
+    }
+}
---
+++ View-Latihan1.php
@@ -0,0 +1,12 @@
+<html>
+    <head> <title>Latihan1</title>
+</head>
+
+<body>
+    Halo Kawan .. Yuk Kita Belajar Web Progamming ..! ! !<br>
+    nilai1 = <?= $nilai1; ?>
+    nilai2 = <?= $nilai2; ?>
+    Ini Hasil Dari Permodelan Dengan Methode Penjumlahan Yaitu <?= $nilai1 . " + " .$nilai2 . " = " .
+    $hasil; ?>
+</body>
+</html>
